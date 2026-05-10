0c590f111b575fc33d34724103e25ebba7c75678
Davidson-09 <davidsonakra@gmail.com>
2026-05-10 15:08:36 +0100

    Revert "supermarket thinz"
    This reverts commit 3ab190d4b87b668bad98aea4b0ce52b6f2dd48c5.

--- app/(frontend)/supermarket/layout.tsx
+++ app/(frontend)/supermarket/layout.tsx
@@ -3,7 +3,6 @@
 import React from 'react';
 import { Navbar } from './components/layout/Navbar';
 import { Footer } from './components/layout/Footer';
-import FloatingCart from '@/app/(frontend)/components/brand/FloatingCart';
 
 interface SupermarketLayoutProps {
   children: React.ReactNode;
@@ -15,7 +14,6 @@ export default function SupermarketLayout({ children }: SupermarketLayoutProps) 
     <div className="min-h-screen flex flex-col bg-white">
       <Navbar />
       <main className="flex-1">{children}</main>
-      <FloatingCart storeSlug="supermarket" accentColor="#f97316" />
       <Footer />
     </div>
   );
